79c7fbf0bf88513b66d9beab928045e8ef54d4dc
Galilea Naomi Martinez Torres <114267832+Galilea5@users.noreply.github.com>
2024-02-12 21:35:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
-const express =require('express');
+const express = require('express');
 const app = express();
 
 const port = 3080;
-const contributors = [
-{
-    "id" : 1,
-    "username": "Galilea"
 
+let alumnos = [
+{
+"id": 1,
+"nombre": "Guillermo",
+"username": "gguillermo"
 },
 {
-    "id" :2,
-    "username":"Naomi"
+"id": 2,
+"nombre": "Galilea",
+"username": "ggalilea"
 },
 {
-    "id" :3,
-    "username":"Gael"
+"id": 3,
+"nombre": "Hector",
+"username": "hhector"
 }
-]
-app.listen(port , ()=>{console.log(`Server is running on ${port}`)});
-app.get('/',(req,res) =>{
-    res.send("Funciona mundo")
+];
+
+app.listen(port, () => {
+console.log(`El servidor está corriendo en el puerto ${port}`);
+});
+
+app.get('/', (req, res) => {
+res.send("¿Funciona?");
+});
+
+app.get("/alumnos", (req, res) => {
+res.send(alumnos);
+});
+
+app.post("/alumnos", (req, res) => {
+const { body } = req;
+alumnos.push(body);
+res.send("Se ha agregado un alumno");
+});
+
+app.patch("/alumnos/:id", (req, res) => {
+const { id } = req.params;
+const { nombre } = req.body;
+
+let alumno = alumnos.find(alumno => alumno.id == id);
+if (alumno) {
+alumno.nombre = nombre;
+res.send({ message: "Se actualizó el nombre del alumno" });
+} else {
+res.status(404).send({ message: "Alumno no encontrado" });
 }
-)
+});
+
+app.delete("/alumnos/:id", (req, res) => {
+const { id } = req.params;
+alumnos = alumnos.filter(alumno => alumno.id != id);
+res.send({ message: "Alumno eliminado" });
+});
+
+
